377be1d743c0860fb9ad6c81539d3db64c6cc773
AliDevsinn <alidevsinn@gmail.com>
2025-11-01 15:36:21 +0500

    update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2709570727"
+    serviceWorkerVersion: "2359515082"
   }
 });
